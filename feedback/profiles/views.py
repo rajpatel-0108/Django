f9473f84a507e544f164a616dfc9b525bd72d840
rajpatel-0108 <110373284+rajpatel-0108@users.noreply.github.com>
2022-08-22 11:42:14 +0530

    Add files via upload

---
+++ feedback/profiles/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+from .forms import ProfileForm
+from .models import UserProfile
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+
+# Create your views here.
+# it is used without models
+# def store_file(file):
+#     with open("temp/image.jpg", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
+
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model= UserProfile
+    fields = "__all__"
+    success_url="/profiles"
+
+class ProfileView(ListView):
+    model= UserProfile
+    template_name="profiles/user_profile.html"
+    context_object_name="profiles"
+
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             # store_file(request.FILES["image"])        without using models
+#             return HttpResponseRedirect("/profiles")
+        
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
+
