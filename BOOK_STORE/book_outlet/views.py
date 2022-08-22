4ab1e7d760d81344ec53b100e7f29afeabd44582
rajpatel-0108 <110373284+rajpatel-0108@users.noreply.github.com>
2022-08-22 11:47:17 +0530

    Add files via upload

---
+++ BOOK_STORE/book_outlet/views.py
@@ -0,0 +1,31 @@
+from audioop import avg
+from django.shortcuts import get_object_or_404,  render
+from .models import Book
+from django.http import Http404
+from django.db.models import Avg
+
+# Create your views here.
+def index(request):
+    books = Book.objects.all().order_by("title")
+    total_no = books.count()
+    average_rating = books.aggregate(Avg("rating"))
+
+    return render(request,"book_outlet/index.html",{
+        "books": books,
+        "total_no_of_books": total_no,
+        "Average_rating": average_rating
+    })
+
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(id=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book,slug=slug)
+    return render(request, "book_outlet/book_detail.html",{
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
+
+    })
